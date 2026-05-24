f76f98102cf44f8b8d4bb7ead2e51ed720dcc776
michalhagos <mhayele3@gmail.com>
2026-05-23 20:50:44 -0500

    Add private fields name type and extra to Topping class

--- src/main/java/com/pluralsight/models/Topping.java
+++ src/main/java/com/pluralsight/models/Topping.java
@@ -1,4 +1,12 @@
 package com.pluralsight.models;
 
 public class Topping {
+    // the name of the topping
+    private String name;
+    // the type of topping
+    private String type;
+    // whether the customer wants extra of this topping
+    private boolean extra;
+
+
 }
