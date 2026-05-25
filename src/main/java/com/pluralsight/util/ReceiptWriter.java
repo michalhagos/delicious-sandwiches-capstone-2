cc2615603d2e76e493e617d62426908ffb45640b
michalhagos <mhayele3@gmail.com>
2026-05-25 15:04:09 -0500

    Write order summary to receipt file in saveReceipt method

--- src/main/java/com/pluralsight/util/ReceiptWriter.java
+++ src/main/java/com/pluralsight/util/ReceiptWriter.java
@@ -9,8 +9,8 @@ import java.time.format.DateTimeFormatter;
 
 public class ReceiptWriter {
     // generateTimestamp generates the current date and time as a string
-// it's used to create a unique filename for each receipt
-// format is yyyyMMdd-HHmmss. example 20230329-121523
+    // it's used to create a unique filename for each receipt
+    // format is yyyyMMdd-HHmmss. example 20230329-121523
     private static String generateTimestamp() {
         // get the current date and time
         LocalDateTime now = LocalDateTime.now();
@@ -20,8 +20,8 @@ public class ReceiptWriter {
         return now.format(formatter);
     }
     // this saves the completed order to a receipt file
-// the file is named using the current date and time
-// saved to the receipts folder inside resources
+    // the file is named using the current date and time
+    // saved to the receipts folder inside resources
     public static void saveReceipt(Order order) {
         try {
             // build the file path using the timestamp as the filename
@@ -29,7 +29,10 @@ public class ReceiptWriter {
             String fileName = "src/main/resources/receipts/" + generateTimestamp() + ".txt";
             // opening the file for writing
             BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
-
+            // write the full order summary to the receipt file
+            // getOrderSummary already has all the items and total price formatted
+            writer.write(order.getOrderSummary());
+            writer.newLine();
 
         } catch (Exception e) {
             System.out.println("Error saving receipt: " + e.getMessage());
