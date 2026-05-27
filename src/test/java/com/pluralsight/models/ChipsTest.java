05d367b9511dfc39bae3d9c819f6f6b03b7be2fd
michalhagos <mhayele3@gmail.com>
2026-05-26 19:19:28 -0500

    Added unit test for Chip class functionalities

--- src/test/java/com/pluralsight/models/ChipsTest.java
+++ src/test/java/com/pluralsight/models/ChipsTest.java
@@ -1,7 +1,33 @@
 package com.pluralsight.models;
-
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ChipsTest {
-
+    @Test
+    public void getPrice_should_alwaysReturnOneFifty() {
+        // arrange
+        Chips chips = new Chips("lays");
+        // act
+        double price = chips.getPrice();
+        // assert
+        assertEquals(1.50, price);
+    }
+    @Test
+    public void getSummary_should_showChipType() {
+        // arrange
+        Chips chips = new Chips("doritos");
+        // act
+        String summary = chips.getSummary();
+        // assert
+        assertTrue(summary.contains("doritos"));
+    }
+    @Test
+    public void getSummary_should_showCorrectPrice() {
+        // arrange
+        Chips chips = new Chips("lays");
+        // act
+        String summary = chips.getSummary();
+        // assert
+        assertTrue(summary.contains("1.50"));
+    }
 }
